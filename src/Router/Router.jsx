d0843f96effe017b9501eda0e774700e6f989dce
KhamdamovAva <73744057+Number1ava@users.noreply.github.com>
2024-11-07 12:54:13 +0500

    added lessonsPage

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import HomePage from '../pages/HomePage'
+import LessonsPage from '../pages/LessonsPage'
 
 function Router() {
   return (
     <>
-    <Routes>
-        <Route index element={<HomePage />}/>
-    </Routes>
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path='/lessons' element={<LessonsPage />} />
+      </Routes>
     </>
   )
 }
