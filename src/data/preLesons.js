7d8c0e8c19817dafca8b2189159dd8120348712c
KhamdamovAva <73744057+Number1ava@users.noreply.github.com>
2024-11-07 14:05:01 +0500

    i changed Home, Navbar, Container, LessonPage

---
+++ src/data/preLesons.js
@@ -0,0 +1,18 @@
+
+export const PreLessons = [
+  {
+    id: 1,
+    title: "the quick fox jumps over the lazy dog",
+    text: "Things on a very small scale behave like nothing",
+  },
+  {
+    id: 2,
+    title: "the quick fox jumps over the lazy dog",
+    text: "Things on a very small scale behave like nothing",
+  },
+  {
+    id: 3,
+    title: "the quick fox jumps over the lazy dog",
+    text: "Things on a very small scale behave like nothing",
+  }
+]
--- src/pages/LessonsPage.jsx
+++ src/pages/LessonsPage.jsx
@@ -2,10 +2,8 @@ import React from 'react'
 
 function LessonsPage() {
   return (
-    <div>LessonsPage
-      <p>Test for brench</p>
-    </div>
-
+    <>
+    </>
   )
 }
 
