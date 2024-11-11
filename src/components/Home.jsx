70e3da5b4c61a6004056cc26ccda97e063816489
KhamdamovAva <73744057+Number1ava@users.noreply.github.com>
2024-11-11 15:58:50 +0500

    made new section-[AboutUs] and wrote slider

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,35 +1,38 @@
-import React from 'react'
-import Navbar from './Navbar'
-import headerImg from '../assets/headerImg.png'
-import { Link } from 'react-router-dom'
-import { PreLessons } from '../data/preLesons'
-   
+import React from 'react';
+import Navbar from './Navbar';
+import headerImg from '../assets/headerImg.png';
+import { Link } from 'react-router-dom';
+import { PreLessons } from '../data/preLesons';
+import { aboutUsSlider } from '../data/aboutUs';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.css';
+
 function Home() {
   const headerChallenge = "px-[50px] py-[10px] border text border-white rounded-md font-bold";
-  const hoverChallenge = "transition-all ease-in delay-[0.5] hover:bg-[#F3CD03] hover:text-[#26335D]"
+  const hoverChallenge = "transition-all ease-in delay-[0.5] hover:bg-[#F3CD03] hover:text-[#26335D]";
   const navLogin = "px-[40px] py-[10px] border text border-white rounded-md font-bold";
-  const hoverLogin = "transition-all ease-in delay-[0.5] hover:bg-[#F3CD03] hover:text-[#26335D]"
-  
+  const hoverLogin = "transition-all ease-in delay-[0.5] hover:bg-[#F3CD03] hover:text-[#26335D]";
+
   return (
     <>
       <header className='container min-h-[100vh]'>
         <Navbar />
-          <div className='grid grid-cols-2 place-items-center pt-[70px]'>
-            <div className='pr-[80px]'>
-              <p className='text-[#F3CD03] font-sans font-bold'>For Better Future</p>
-              <h1 className='text-[3.625rem] font-bold w-[450px] font-sans py-[25px]'>HIGH QUALITY LESSONS</h1>
-              <p className='w-[300px] pb-[30px]'>Find the right instructor for you from over 10,000 teachers</p>
-              <div>
-                <button className={`${headerChallenge} ${hoverChallenge}`}>Challenge</button>
-              </div>
+        <div className='grid grid-cols-2 place-items-center pt-[70px]'>
+          <div className='pr-[80px]'>
+            <p className='text-[#F3CD03] font-sans font-bold'>For Better Future</p>
+            <h1 className='text-[3.625rem] font-bold w-[450px] font-sans py-[25px]'>HIGH QUALITY LESSONS</h1>
+            <p className='w-[300px] pb-[30px]'>Find the right instructor for you from over 10,000 teachers</p>
+            <div>
+              <button className={`${headerChallenge} ${hoverChallenge}`}>Challenge</button>
             </div>
-              <div>
-                <img src={headerImg} alt="" />
-              </div>
-      </div>
-     </header>
+          </div>
+          <div>
+            <img src={headerImg} alt="" />
+          </div>
+        </div>
+      </header>
       <main>
-        <section className='container min-h-[600px]' >
+        <section className='container min-h-[600px]' id='Lessons'>
           <div className='py-[60px] text-center'>
             <h2 className='text-[40px] font-[700]'>Our Main Lessons</h2>
             <p className='text-[#F3CD03]'>For Better Future</p>
@@ -49,9 +52,36 @@ function Home() {
             </Link>
           </div>
         </section>
+        <section className='container min-h-[600px]' id='AboutUs'>
+          <div className='pt-[40px] mb-[60px] text-center'>
+            <h2 className='text-[40px] font-[700]'>About us</h2>
+            <p className='text-[#F3CD03]'>Learn, Grow, Achieve</p>
+          </div>
+          <div className='w-full max-w-[706px]  mx-auto'>
+            <Swiper
+              spaceBetween={30}
+              slidesPerView={1}
+              loop={true}
+              autoplay={{ delay: 3000 }}
+              pagination={{ clickable: true }}
+            >
+              {aboutUsSlider.map((subject) => (
+                <SwiperSlide key={subject.id}>
+                  <div className='flex justify-between my-[10px] min-h-[270px] bg-white p-[30px] rounded-[19px] space-x-6'>
+                    <img className='w-[250px]' alt="About us, Icon" src={subject.image} />
+                    <div>
+                      <h4 className='text-[22px] text-[#F3CD03] font-bold text-center mb-[6px]'>{subject.title}</h4>
+                      <p className='text-[#4D4D4D] text-[14px] '>{subject.description}</p>
+                    </div>
+                  </div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+        </section>
       </main>
     </>
   )
 }
 
-export default Home
+export default Home;
