bf2e322a6e08ddd887361b0e1fbdea70296956ff
KhamdamovAva <73744057+Number1ava@users.noreply.github.com>
2024-11-11 16:04:16 +0500

    created data file - aboutUs.js

---
+++ src/data/aboutUs.js
@@ -0,0 +1,38 @@
+import aboutUsIcon1 from '../assets/aboutUsIcon1.svg'
+import aboutUsIcon2 from '../assets/aboutUsIcon2.svg'
+import aboutUsIcon3 from '../assets/aboutUsIcon3.svg'
+import aboutUsIcon4 from '../assets/aboutUsIcon4.svg'
+import aboutUsIcon5 from '../assets/aboutUsIcon5.svg'
+
+
+export const aboutUsSlider = [{
+  id: 1,
+  image: aboutUsIcon1,
+  title: "About Our Product",
+  description: "We are a team of developers and educators dedicated to making programming understandable and accessible. On our platform, you'll find courses, hands-on exercises, and JavaScript projects that immerse you in the world of coding. Whether you're a beginner or an advanced developer, we offer the tools and challenges to help you grow your skills and confidence."
+},
+{
+  id: 2,
+  image: aboutUsIcon2,
+  title: "Interactive Exercises",
+  description: "Our interactive exercises make learning engaging and hands-on, allowing you to apply theory immediately and see results in real time. Each task is designed to reinforce your understanding, helping you master JavaScript faster, avoid common pitfalls, and build a strong foundation in coding"
+},
+{
+  id: 3,
+  image: aboutUsIcon3,
+  title: "Personalized Learning",
+  description: "Our platform adapts to your unique skill set, whether you're just beginning or already have coding experience. With personalized courses and challenges, you'll be guided through each stage of learning, allowing you to grow your JavaScript skills at a comfortable pace. Each step is designed to match your progress, helping you build confidence, master key concepts, and tackle increasingly complex problems as you advance."
+},
+{
+  id: 4,
+  image: aboutUsIcon4,
+  title: "Real projects",
+  description: "Put your JavaScript skills to the test by working on practical, real-world tasks that simulate the challenges faced by IT professionals. These hands-on projects will help you gain valuable experience and build a strong portfolio that showcases your abilities. Whether you're creating web applications or solving complex problems, you'll gain insight into the development process and improve your readiness for future job opportunities in tech."
+},
+{
+  id: 5,
+  image: aboutUsIcon5,
+  title: "Communnity Support",
+  description: "Become part of a supportive community of fellow learners and experienced developers! Exchange ideas, get valuable advice, and share your progress as you work towards mastering JavaScript. Whether you're looking for help with a tricky problem or offering guidance to others, our community fosters collaboration and growth. Together, we can solve challenges, celebrate achievements, and help each other advance in the world of programming."
+},
+];
