f574e01e6ab5c2593e4ae1bb06911739522fe792
KhamdamovAva <73744057+Number1ava@users.noreply.github.com>
2024-11-12 12:19:58 +0500

    added new id for ContactUs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,10 +5,10 @@ import { Link } from 'react-router-dom';
 function Navbar() {
   const navItem = "py-[5px] border-b-2 border-transparent";
   const navJoin = "bg-[#F3CD03] px-[21px] py-[11px] rounded-md text-[#26335D] text-sm font-bold";
-  const itemHover = "transition-all ease-in delay-[0.6] hover:border-[#F3CD03]";
+  const itemHover = "transition-all ease-in delay-[0.6] hover:border-[#F3CD03] ease-in duration-300";
   const navLogin = "px-[20px] py-[10px] border text border-white rounded-md font-bold";
-  const hoverLogin = "transition-all ease-in delay-[0.5] hover:bg-[#F3CD03] hover:text-[#26335D]"
-  const hoverJoin = "transition-all ease-in delay-[0.5] border border-white hover:bg-transparent hover:text-white"
+  const hoverLogin = "transition-all ease-in delay-[0.5] hover:bg-[#F3CD03] hover:text-[#26335D] ease-in duration-300"
+  const hoverJoin = "transition-all ease-in delay-[0.5] border border-white hover:bg-transparent hover:text-white ease-in duration-300"
 
   return (
     <>
@@ -20,7 +20,7 @@ function Navbar() {
           <a href="#" className={`${navItem} ${itemHover}`}>Home</a>
           <a href="#Lessons" className={`${navItem} ${itemHover}`}>Lessons</a>
           <a href="#AboutUs" className={`${navItem} ${itemHover}`}>About Us</a>
-          <a href="#" className={`${navItem} ${itemHover}`}>Contact Us</a>
+          <a href="#ContactUs" className={`${navItem} ${itemHover}`}>Contact Us</a>
         </div>
         <div className="flex gap-[30px] p-[10px]">
           <button><a href="#" className={`${navLogin} ${hoverLogin}`}>Login</a></button>
